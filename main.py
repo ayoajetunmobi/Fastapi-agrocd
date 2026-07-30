c9061be5153517c836d64b9cd5dd0330a2bdb676
ayoajetunmobi <ayoajetunmobi78@gmail.com>
2026-07-30 13:22:50 +0000

    few changes

--- main.py
+++ main.py
@@ -6,10 +6,9 @@ app = FastAPI()
 # Define a root route
 @app.get("/")
 def read_root():
-    return {"message": "Hello, World!"}
+    return {"message": "APP STARTED JUST NOW !!!"}
 
 # Define a route with path and query parameters
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str = None):
     return {"item_id": item_id, "query_q": q}
-
